0ff679ca9b9db1435eed3b967f6d9a0a98aef757
okoala <dapixp@gmail.com>
2016-03-19 12:14:57 +0800

    调整下本地开发IP

--- ios/ThreetyDaysofReactNative/AppDelegate.m
+++ ios/ThreetyDaysofReactNative/AppDelegate.m
@@ -33,7 +33,7 @@
    * on the same Wi-Fi network.
    */
 #ifdef DEBUG
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.100:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://127.0.0.1:8081/index.ios.bundle?platform=ios&dev=true"];
 #else
   jsCodeLocation = [CodePush bundleURL];
 #endif
